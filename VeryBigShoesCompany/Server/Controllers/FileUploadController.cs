c668726df5af9735f5b271edb3308f1002214897
vasi30 <vasi30@gmail.com>
2021-10-30 23:39:23 +0300

    add project

---
+++ VeryBigShoesCompany/Server/Controllers/FileUploadController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+using VeryBigShoesCompany.Shared;
+
+namespace VeryBigShoesCompany.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileUploadController : ControllerBase
+    {
+        private readonly IWebHostEnvironment environment;
+
+        public FileUploadController(IWebHostEnvironment environment)
+        {
+            this.environment = environment;
+        }
+
+        [HttpPost]
+        public void Post(UploadedFile uploadedFile)
+        {
+            var path = $"{environment.WebRootPath}\\{uploadedFile.FileName}";
+
+            var memoryStream = new MemoryStream(uploadedFile.FileContent);
+            XmlReader xmlReader = XmlReader.Create(memoryStream);
+            XDocument doc = XDocument.Load(xmlReader);
+
+            ValidateXml(doc);
+
+            BigShoeDataImport data = null;
+            XmlSerializer serializer = new XmlSerializer(typeof(BigShoeDataImport));
+
+            data = (BigShoeDataImport)serializer.Deserialize(doc.Root.CreateReader());
+        }
+
+        void ValidateXml(XDocument doc)
+        {
+            XmlSchemaSet schema = new XmlSchemaSet();
+            schema.Add("", $"{environment.WebRootPath}\\Resources\\OrderImport.xsd");
+            doc.Validate(schema, ValidationEventHandler);
+        }
+
+        static void ValidationEventHandler(object sender, ValidationEventArgs e)
+        {
+            XmlSeverityType type = XmlSeverityType.Warning;
+            if (Enum.TryParse<XmlSeverityType>("Error", out type))
+            {
+                if (type == XmlSeverityType.Error) throw new Exception(e.Message);
+            }
+        }
+    }
+}
---
+++ VeryBigShoesCompany/Server/Controllers/Order.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace VeryBigShoesCompany.Server.Controllers
+{
+    [Serializable()]
+    public class Order
+    {
+        [XmlAttribute(nameof(CustomerName))]
+        public string CustomerName { get; set; }
+        
+        [XmlAttribute(nameof(CustomerEmail))]
+        public string CustomerEmail { get; set; }
+        
+        [XmlAttribute(nameof(Quantity))]
+        public string Quantity { get; set; }
+        
+        [XmlAttribute(nameof(Notes))]
+        public string Notes { get; set; }
+        
+        [XmlAttribute(nameof(Size))]
+        public string Size { get; set; }
+        
+        [XmlAttribute(nameof(DateRequired))]
+        public string DateRequired { get; set; }
+    }
+
+    [Serializable]
+    [XmlRoot("BigShoeDataImport")]
+    public class BigShoeDataImport
+    {
+        [XmlElement("Order")]
+        public Order[] Orders { get; set; }
+    }
+}
